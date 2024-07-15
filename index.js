6631c8fc99b2723ba355c7c5cc39d9bdd42cb742
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2024-07-15 18:37:26 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// The entry point for the React application. Renders the App component into the root DOM node.
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
